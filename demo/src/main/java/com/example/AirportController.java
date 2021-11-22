bf57879d760c0d3ac1aae78e37af4ae5cae86ca8
Henok Abraha <henokabraha@Henoks-MacBook-Air.local>
2021-11-22 20:42:39 +0300

    change login

--- demo/src/main/java/com/example/AirportController.java
+++ demo/src/main/java/com/example/AirportController.java
@@ -45,10 +45,8 @@ public class AirportController{
         return "logIn.html";
     }
     
-    
+ 
 
-
-    
 }
 
 
--- demo/src/main/java/com/example/AirportModel.java
+++ demo/src/main/java/com/example/AirportModel.java
@@ -14,7 +14,21 @@ public class AirportModel {
     private String firstName;
     private String lastName;
     private String phoneNumber;
+    private String userName;
+    private String password;
     
+    public String getUserName() {
+        return userName;
+    }
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
     public int getUserId() {
         return userId;
     }
